102d774b56c5bb5c2dcfd38cfb87de658f971fc7
luist23 <00127717@uca.edu.sv>
2020-11-24 20:36:32 -0600

    finish menu start

--- keyboarEvent.cpp
+++ keyboarEvent.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <winuser.h>
+#include <cstdlib>
 //#include <conio.h>
 
 using namespace std;
@@ -15,52 +16,70 @@ void __fastcall onKey::Edit1KeyPress(TObject *Sender, System::WideChar &Key)
 
 int main(){
 
-    char key;
-    bool key_up = false;
-    int flag = 0;
-bool loop = true;
-wellcome(1);
-while(loop && flag < 10){
-    //cin >> key;
-    /*
-    if(key == "WHATEVER MATCHED"){
-        loop = false;
-    } 
-    //*/
-    //cout<<"neter \n";
-    //getch();
-    //if(kbhit())break;
-    //cout<<key;
-    //key_up = GetKeyState(VK_UP) & 0x8000;
-    if(  GetKeyState(VK_UP) & 0x8000 ){
-        cout<<"up"<< flag << endl;
-        //key_up = false;
-        //keybd_event(VkKeyScan('A'),0x9e,0,0);
-        flag++;
-        Sleep(100);
+    //char key;
+    //bool key_up = false;
+    int flag = 1;       //opcion actual del menu
+    int sizeOption=2;   //numero total de opciones en el menu
+    bool loop = true;   //bandera para el while true
+    wellcome(   flag);  //invocacion del menu
+    while(loop){
 
+        //evento de key Up
+        if(  GetKeyState(VK_UP) & 0x8000 ){
+            if(flag > 1){
+                flag--;
+                wellcome(flag);
+            }
+            Sleep(100);//para corregir fallo de while
+        }
+        //*evento de key DOWN
+         if( GetAsyncKeyState(VK_DOWN) & 0x8000){
+             if(flag < sizeOption){
+                 flag++;
+                 wellcome(flag);
+             }
+            Sleep(100);//para corregir fallo de while
+        }
+
+         //evento de ENTER seleccionando una opcion de menu
+        if( GetAsyncKeyState(VK_RETURN) & 0x8000){
+            switch (flag) {
+                case 1:
+                    cout<<"new game start :v"<<endl;
+                    break;
+                case 2:
+                    loop=false;
+                    cout<<"exit... :'v"<<endl;
+                    break;
+                default:
+                    break;
+            }
+
+            Sleep(100);//para corregir fallo de while
+        }
+         //*/
+         Sleep(10);//para corregir fallo de while
     }
-    //*
-     if( GetAsyncKeyState(VK_DOWN) & 0x8000){
-        cout<<"down"<< flag<< endl;
-        flag++;
-        Sleep(100);
-    }
-     //*/
-     Sleep(10);
-}
 
     return 0;
 }
 
 void wellcome(int opc){
-    cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl;
-    cout<<"\t\t\t\t\t\tWELLCOME !! TO ......!!!\n";
+    //system("cls");//linux
+    system("clear");//windows
+    cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
+    cout<<"\t\t\t\t\t\tWELLCOME !! TO ......!!!\n\n";
     cout<<"\t\t\t=====\t===    =="   <<"\t=======\t  ==  =="  <<"\t====="   <<endl;
     cout<<"\t\t\t||   \t||\\\\   ||" <<"\t||   ||\t  || // "  <<"\t||"      <<endl;
     cout<<"\t\t\t=====\t|| \\\\  ||" <<"\t||===||\t  |||   "  <<"\t====="   <<endl;
     cout<<"\t\t\t   ||\t||  \\\\ ||" <<"\t||   ||\t  || \\\\ "<<"\t||"      <<endl;
     cout<<"\t\t\t=====\t==   ===="   <<"\t==   ==\t  ==  =="  <<"\t====="   <<endl;
+    cout<<endl<<endl;
+    cout<<"\t\t\t\t\t\t"<<((opc==1)?"*":"")<<" START";
+    cout<<endl;
+    cout<<"\t\t\t\t\t\t"<<((opc==2)?"*":"")<<" EXIT";
+    cout<<endl<<endl;
+    cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
 }
 
 

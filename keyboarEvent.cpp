a185ec8fe0d58faf5f376167595c7892c570a08f
luist23 <00127717@uca.edu.sv>
2020-11-24 21:31:34 -0600

    cambiando keyboar event

--- keyboarEvent.cpp
+++ keyboarEvent.cpp
@@ -3,6 +3,9 @@
 #include <windows.h>
 #include <winuser.h>
 #include <cstdlib>
+
+#include <termios.h>
+#include <unistd.h>
 //#define mySleep(x) Sleep(x);
 
 // #elif defined __unix__ /* Creo que es esta macro, deberia estar definida en sistemas Unix-like*/
@@ -29,33 +32,44 @@ void __fastcall onKey::Edit1KeyPress(TObject *Sender, System::WideChar &Key)
 
 int main(){
 
-    //char key;
+    char key;
     //bool key_up = false;
     int flag = 1;       //opcion actual del menu
     int sizeOption=2;   //numero total de opciones en el menu
     bool loop = true;   //bandera para el while true
+    Sleep(100);//para corregir fallo de while
     wellcome(   flag);  //invocacion del menu
+
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    //*/
+
     while(loop){
+        key=getchar();//up:65 down:66 left:68 rigth:67
+        //cout<<int(key);
 
         //evento de key Up
-        if(  GetKeyState(VK_UP) & 0x8000 ){
+        if(  /*GetKeyState(VK_UP) & 0x8000*/ key==65 ){
             if(flag > 1){
                 flag--;
-                wellcome(flag);
+                //wellcome(flag);
             }
             Sleep(100);//para corregir fallo de while
         }
         //*evento de key DOWN
-         if( GetAsyncKeyState(VK_DOWN) & 0x8000){
+         if( /*GetAsyncKeyState(VK_DOWN) & 0x8000*/ key=='B'){
              if(flag < sizeOption){
                  flag++;
-                 wellcome(flag);
+                 //wellcome(flag);
              }
-            Sleep(100);//para corregir fallo de while
+            //Sleep(100);//para corregir fallo de while
         }
-
+        wellcome(flag);
          //evento de ENTER seleccionando una opcion de menu
-        if( GetAsyncKeyState(VK_RETURN) & 0x8000){
+        if( /*GetAsyncKeyState(VK_RETURN) & 0x8000*/ key=='\n'){
+            //Sleep(100);//para corregir fallo de while
             switch (flag) {
                 case 1:
                     cout<<"new game start :v"<<endl;
@@ -68,16 +82,26 @@ int main(){
                     break;
             }
 
-            Sleep(100);//para corregir fallo de while
+            //Sleep(100);//para corregir fallo de while
         }
          //*/
+
          Sleep(10);//para corregir fallo de while
     }
+
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    //*/
 
     return 0;
 }
 
 void wellcome(int opc){
+    //system("cls");//linux
+    cout<<flush;
+    system("clear");//windows
+    Sleep(10);//para corregir fallo de while
     cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl;
     cout<<"\t\t\t\t\t\tWELLCOME !! TO ......!!!\n";
     cout<<"\t\t\t=====\t===    =="   <<"\t=======\t  ==  =="  <<"\t====="   <<endl;

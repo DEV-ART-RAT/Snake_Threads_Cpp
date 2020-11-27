a46b762e98de2177cd14c8fac6e07c7d4e73e43f
luist23 <00127717@uca.edu.sv>
2020-11-27 16:00:28 -0600

    simplificando menus

--- views/game_over.cpp
+++ views/game_over.cpp
@@ -6,64 +6,12 @@
 #include "../snake/keyEvent.h"
 #include "./windows_start.cpp"
 
-
-
 using namespace std;
 struct termios term_over;
-//void abrir_buffer();
-//void cerrar_buffer();
-void game_over_Print(int opc);
 
-int game_over(node<nodeuserinfouser>* userdata) {
-    char key_game_over;
-    int flag_game_over = 1;       //opcion actual del menu
-    int sizeOption_game_over=2;   //numero total de opciones en el menu
-    bool loop_game_over = true;   //bandera para el while true
-    sleep(0.1);//para corregir fallo de while
-    game_over_Print(flag_game_over);
-    //cin.ignore();
-    abrir_buffer();
-    while(loop_game_over){
-        key_game_over=getchar();//up:65 down:66 left:68 rigth:67
-        if(  /*GetKey_game_overState(VK_UP) & 0x8000*/ key_game_over==65 ){
-            if(flag_game_over > 1){
-                flag_game_over--;
-            }
-        }
-        if( /*GetAsyncKey_game_overState(VK_DOWN) & 0x8000*/ key_game_over=='B'){
-            if(flag_game_over < sizeOption_game_over){
-                flag_game_over++;
-                 //wellcome(flag_game_over);
-            }
-        }
-        game_over_Print(flag_game_over);
-        if( /*GetAsyncKey_game_overState(VK_RETURN) & 0x8000*/ key_game_over=='\n'){
-            //Sleep(100);//para corregir fallo de while
-            switch (flag_game_over) {
-                case 1:
-                    loop_game_over=false;
-                    startMenuOpc(userdata,startMenu(userdata));
-                    break;
-                case 2:
-                    loop_game_over=false;
-                    exit(EXIT_SUCCESS);
-                    break;
-                default:
-                    break;
-            }
+int startMenuAux(node<nodeuserinfouser>* , auto ,int );
 
-            //Sleep(100);//para corregir fallo de while
-        }
-         //*/
-
-        sleep(0.01);//para corregir fallo de while
-    }
-
-    cerrar_buffer();
-    return 0;
-};
-
-void game_over_Print(int opc){
+auto mensajeGameOver = [](int opc,node<nodeuserinfouser>* userdata) { 
     CLEAR;
     cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
     cout<<"\t\t\t=====   ===    =="   <<"   =======   ==  =="  <<"   ====="   <<endl;
@@ -78,5 +26,22 @@ void game_over_Print(int opc){
     cout<<endl;
     cout<<"\t\t\t\t\t"<<((opc==2)?"*":" ")<<"  EXIT "<<((opc==2)?"*":"");
     cout<<endl<<endl;
-}
+};
 
+int gameOverMenuAux(node<nodeuserinfouser>* userdata, int flag) {
+    switch (flag) {
+        case 1:
+            startMenu(userdata);
+            break;
+        case 2:
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            break;
+    }
+};
+
+int gameOverMenu(node<nodeuserinfouser>* userdata){
+    gameOverMenuAux(userdata,startMenuAux(userdata,mensajeGameOver,2));
+    return 0;
+}
--- views/windows_start.cpp
+++ views/windows_start.cpp
@@ -22,74 +22,8 @@
 using namespace std;
 
 int welcomemain(int &P,int&C,string user);
-void wellcome(int,node<nodeuserinfouser>* );
 
-
-int startMenu(node<nodeuserinfouser>* userdata){
-    char key; //contenedor de tecla
-    int flag = 1;       //opcion actual del menu
-    int sizeOption=3;   //numero total de opciones en el menu
-    bool loop = true;   //bandera para el while true
-
-    sleep(0.1);//para corregir fallo de while
-    wellcome(flag,userdata);  //invocacion del menu
-
-    abrir_buffer();
-    sleep(0.1);//para corregir fallo de whiles
-    
-    
-    while(loop){
-        key=getchar();//up:65 down:66 left:68 rigth:67
-
-        if(key==65 ){
-            if(flag > 1){
-                flag--;
-            }
-        }
-        if(  key=='B'){
-            if(flag < sizeOption){
-                flag++;
-            }
-        }
-        wellcome(flag,userdata);
-        if( key=='\n'){
-            cerrar_buffer();
-            return flag;
-        }
-
-        sleep(0.01);//para corregir fallo de while
-    }
-
-    cerrar_buffer();
-    //*/
-
-    return 0;
-}
-
-int startMenuOpc(node<nodeuserinfouser>* userdata, int flag){
-    switch (flag) {
-        case 1:
-            cout<<"new game start :v"<<endl;
-            //playmatrix(&snake);
-            welcomedifficulty(userdata);
-            //playmatrix(&snake);
-            game_over(userdata);
-            // loop=false; //para que termine el juego sin que se regrese al menu :
-            break;
-        case 2:
-            welcomeshop(userdata);
-            break;
-        case 3:
-            cout<<"exit... :'v"<<endl;
-            exit(EXIT_SUCCESS);
-            break;
-        default:
-            break;
-    }
-    return 0;
-}
-
-void wellcome(int opc, node<nodeuserinfouser>* userdata){
+auto mensajeStart = [](int opc, node<nodeuserinfouser>* userdata) { 
     CLEAR;
     cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
     cout<<"\t\t\t\tWELLCOME !!  ¡¡¡"<<userdata->info.name<<"!!! TO ......!!!\n\n";
@@ -106,7 +40,80 @@ void wellcome(int opc, node<nodeuserinfouser>* userdata){
     cout<<"\t\t\t\t\t"<<((opc==3)?"*":" ")<<"  EXIT "<<((opc==3)?"*":"");
     cout<<endl<<endl;
     cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
+};
+
+
+int startMenuAux(node<nodeuserinfouser>* userdata, auto wellcome,int sizeOption){
+    char key; //contenedor de tecla
+    int flag = 1;       //opcion actual del menu
+    //int sizeOption=3;   //numero total de opciones en el menu
+    bool loop = true;   //bandera para el while true
+
+    sleep(0.1);//para corregir fallo de while
+    wellcome(flag,userdata);  //invocacion del menu
+
+    cin.ignore();
+    //sleep(0.1);//para corregir fallo de whiles
+
+    while(loop){
+
+        key=getchar();//up:65 down:66 left:68 rigth:67
+
+        if(key==65 ){
+            if(flag > 1){
+                flag--;
+            }
+        }
+        
+        if(  key=='B'){
+            if(flag < sizeOption){
+                flag++;
+            }
+        }
+
+        if( key=='\n'){
+            return flag;
+        }
+
+        wellcome(flag,userdata);
+        sleep(0.01);//para corregir fallo de while
+    }
+    cin.ignore();
+
+    return 0;
 }
+
+
+
+int startMenuOpc(node<nodeuserinfouser>* userdata, int flag){
+    switch (flag) {
+        case 1:
+            //cout<<"new game start :v"<<endl;
+            //playmatrix(&snake);
+            difficultyMenu(userdata);
+            //playmatrix(&snake);
+            gameOverMenu(userdata);
+            // loop=false; //para que termine el juego sin que se regrese al menu :
+            break;
+        case 2:
+            welcomeshop(userdata);
+            break;
+        case 3:
+            cout<<"exit... :'v"<<endl;
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+int startMenu(node<nodeuserinfouser>* userdata){
+    startMenuOpc(userdata,startMenuAux(userdata,mensajeStart,3));
+    return 0;
+}
+
+
 
 
 

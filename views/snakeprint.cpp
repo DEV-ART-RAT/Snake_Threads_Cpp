bb814bdde6dee0a792f250c2c7de3ce7b0bb0f5f
luist23 <00127717@uca.edu.sv>
2020-11-28 13:52:59 -0600

    updsting mensage start

--- views/snakeprint.cpp
+++ views/snakeprint.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include "../tools/screenControl.h"
 #include <iomanip>
+#include <string>
 
 using namespace std;
 int main(void) {
     winsize w = screenSize();//obtenemos las dimensiones de nuestra terminal
-    int tamanio_alto=w.ws_row - 3;
-    int tamanio_largo=w.ws_col /1 ;
-    for (int i = 0; i < tamanio_alto; i++)
+    int tamanio_alto=w.ws_row - 4;
+    int tamanio_largo=w.ws_col -4 ;
+    string linea1[] = {
+        "=====   ===    ==   =======   ==  ==   =====",
+        "||      ||\\\\   ||   ||   ||   || //    ||   ",
+        "====    || \\\\  ||   ||===||   |||      =====",
+        "   ||   ||  \\\\ ||   ||   ||   || \\\\    ||   " ,
+        "=====   ==   ====   ==   ==   ==  ==   =====" 
+    };
+    int space = tamanio_largo/2 - linea1->size();
+    string str;//("lolcat");
+    str.insert(0, space, ' ');
+
+    for (int i=0; i<5;i++){
+        cout<<"*"<<str <<  linea1[i] << str <<"*" <<endl;
+    }
+    //cout<<linea1.size()<<endl;
+    /*for (int i = 0; i < tamanio_alto; i++)
     {
-        /* code */
+        
         if(i>0 && i<6){
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<"=====   ===    =="   <<"   =======   ==  =="  <<"   ====="   <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<"||      ||\\\\   ||" <<"   ||   ||   || // "  <<"   ||"      <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<"=====   || \\\\  ||" <<"   ||===||   |||   "  <<"   ====="   <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<"   ||   ||  \\\\ ||" <<"   ||   ||   || \\\\ "<<"   ||"      <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<"=====   ==   ===="   <<"   ==   ==   ==  =="  <<"   ====="   <<endl;i-=-1;
+        cout<<"*"<<setw((tamanio_largo/2)-5)<<  linea1 << setw((tamanio_largo/2)-5) <<"*" <<endl;i-=-1;
+        cout<<"*"<<setw((tamanio_largo/2)-5)<<      <<endl;i-=-1;
+        cout<<"*"<<setw((tamanio_largo/2)-5)<<   <<endl;i-=-1;
+        cout<<"*"<<setw((tamanio_largo/2)-5)<<     <<endl;i-=-1;
+        cout<<"*"<<setw((tamanio_largo/2)-5)<<  <<endl;i-=-1;
         }
         if (i==0||i==tamanio_alto-1)
         {
@@ -35,7 +51,7 @@ int main(void) {
                 
         }
         
-    }
+    }*/
     
     return 0;
 };

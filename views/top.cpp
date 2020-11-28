3751fd15cc87b23b2714c5e404b631b718b4a030
Drubico <00121117@uca.edu.sv>
2020-11-27 19:01:44 -0600

    (DR-28) ->> TOP View

--- views/top.cpp
+++ views/top.cpp
@@ -7,14 +7,20 @@
 #include "../snake/list.cpp"
 #include "../snake/clear.cpp"
 #include "../user/readuser.cpp"
+#include <iomanip>
 
 using namespace std;
 void top_game(node<nodeuserinfouser>* userdata,doubleLinked<nodeuserinfouser>* userlist);
 void welcometop();
 
+template<class T>
+void printtop(node<T>* front);
+void printtopaux(doubleLinked<nodeuserinfouser>* userlist);
+
 void top_game(node<nodeuserinfouser>* userdata,doubleLinked<nodeuserinfouser>* userlist){
     welcometop();
-    traverseBegin(userlist->front);
+    //traverseBegin(userlist->front);
+    printtopaux(userlist);
     cout<<"presiona ENTER para continuar";
     cin.ignore();
     cin.get();
@@ -31,6 +37,46 @@ void welcometop(){
     cout<<"\t\t\t=====   ==   ===="   <<"   ==   ==   ==  =="  <<"   ====="   <<endl;
     cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
     cout<<endl<<endl;
-    cout<<"\t\t\t TOP :  \n";
 
+}
+template<class T>
+void printtop(node<T>* front){
+    string line,lineone,wall,fwall,lwall;
+    line="=================================================================================================================================";
+    wall="\t|\t";
+    fwall="|\t";
+    lwall="\t|";
+    //cout<<fwall;
+    if(front) {
+        cout<< fwall;
+        cout<<setw(20)<< front->info.name;
+        cout<<setw(20)<< wall;
+        cout<<setw(20)<< front->info.coin;
+        cout<<setw(20)<<wall;
+        cout<<setw(20)<<front->info.puntaje;
+        cout<<setw(20)<<lwall;
+        cout<<endl ;
+        printtop(front->next);
+    }
+    else    
+        cout<<endl;
+}
+
+void printtopaux(doubleLinked<nodeuserinfouser>* userlist){
+    string line,lineone,wall,fwall,lwall,all;int size;
+    wall="\t|\t";
+    fwall="|\t";
+    lwall="\t|";
+    cout<< fwall;
+    cout<<setw(20)<< "Nombre ";
+    cout<<setw(20)<<wall;
+    cout<<setw(20)<< "Monedas";
+    cout<<setw(20)<<wall;
+    cout<<setw(20)<<"Puntaje";
+    cout<<setw(20)<<lwall;
+    cout<<endl ;
+    cout.fill('-') ;
+    cout<<endl ;
+    cout.fill(' ');
+    printtop(userlist->front);
 }

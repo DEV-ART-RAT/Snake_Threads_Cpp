aaf6c6dee866d2ecca9d5043385944687f7a4285
luist23 <00127717@uca.edu.sv>
2020-11-28 14:50:54 -0600

    edit mensajes

--- views/snakeprint.cpp
+++ views/snakeprint.cpp
@@ -5,9 +5,8 @@
 
 using namespace std;
 void mensageSnake(int col,string linea1[],int linea1size){
-
     string str;
-    col = (col- linea1->size() -4 )/2;
+    col = (col- linea1->size() - 4 )/2;
     str.insert(0, col, ' ');
     for (int i=0; i <linea1size ;i++){
         cout<<"*"<<str <<  linea1[i] << str <<"*" <<endl;
@@ -25,6 +24,40 @@ void mensageMargin(int col){
     strl.insert(0, col - 2, '*');
     cout<<strl<<endl;
 }
+
+auto mensajeStart = [](int opc, node<nodeuserinfouser>* userdata) { 
+    //CLEAR;
+    //cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
+    mensageMargin(w.ws_col);
+    mensageSteep(w.ws_col);
+    string linea1[] = {
+        "WELLCOME !!  ¡¡¡" + userdata->info.name + " !!! TO ......!!!"
+    };
+    mensageSnake(w.ws_col,linea1 , 1);
+    string linea2[] = {
+        "=====   ===    ==   =======   ==  ==   =====",
+        "||      ||\\\\   ||   ||   ||   || //    ||   ",
+        "====    || \\\\  ||   ||===||   |||      =====",
+        "   ||   ||  \\\\ ||   ||   ||   || \\\\    ||   " ,
+        "=====   ==   ====   ==   ==   ==  ==   =====" 
+    };
+    mensageSnake(w.ws_col,linea2 , 5);
+    mensageSteep(w.ws_col);
+    mensageSteep(w.ws_col);
+
+
+
+    
+    cout<<"\t\t\t\t\t"<<((opc==1)?"*":" ")<<"  START "<<((opc==1)?"*":"");
+    cout<<endl;
+    cout<<"\t\t\t\t\t"<<((opc==2)?"*":" ")<<" TIENDA "<<((opc==2)?"*":"");
+    cout<<endl;
+    cout<<"\t\t\t\t\t"<<((opc==3)?"*":" ")<<"  TOP "<<((opc==3)?"*":"");
+    cout<<endl;
+    cout<<"\t\t\t\t\t"<<((opc==4)?"*":" ")<<"  EXIT "<<((opc==4)?"*":"");
+    cout<<endl<<endl;
+    cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
+};
 
 
 int main(void) {

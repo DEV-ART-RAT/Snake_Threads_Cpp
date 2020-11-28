2265e1c48d91559436205f8a7d8ad3354da39b82
luist23 <00127717@uca.edu.sv>
2020-11-28 15:39:58 -0600

    edit mensaje star

--- views/windows_start.cpp
+++ views/windows_start.cpp
@@ -30,8 +30,7 @@ void mensageMarginStart(int opc, node<nodeuserinfouser>* userdata) {
     //cout<<"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"<<endl<<endl;
     mensageMargin(w.ws_col);
     mensageSteep(w.ws_col);
-    string linea1 = (userdata->info.name.size() %2 != 0) ? "WELLCOME !!  ¡¡¡" + userdata->info.name + " !!! TO ......!!!"
-    : "WELLCOME !!!  ¡¡¡" + userdata->info.name + " !!! TO F......!!!";
+    string linea1 =  "WELLCOME " + userdata->info.name + "  TO ...";
 
 
     mensageLine(w.ws_col,linea1);

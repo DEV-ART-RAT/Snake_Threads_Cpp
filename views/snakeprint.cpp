73f37c48235c04f33389868991467e869fe5057b
luist23 <00127717@uca.edu.sv>
2020-11-28 14:09:23 -0600

    editasnod mesanje

--- views/snakeprint.cpp
+++ views/snakeprint.cpp
@@ -4,10 +4,8 @@
 #include <string>
 
 using namespace std;
-int main(void) {
-    winsize w = screenSize();//obtenemos las dimensiones de nuestra terminal
-    int tamanio_alto=w.ws_row - 4;
-    int tamanio_largo=w.ws_col -4 ;
+void mensageSnake(int col){
+    string str,strl;
     string linea1[] = {
         "=====   ===    ==   =======   ==  ==   =====",
         "||      ||\\\\   ||   ||   ||   || //    ||   ",
@@ -15,43 +13,20 @@ int main(void) {
         "   ||   ||  \\\\ ||   ||   ||   || \\\\    ||   " ,
         "=====   ==   ====   ==   ==   ==  ==   =====" 
     };
-    int space = (tamanio_largo - linea1->size())/2;
-    string str;//("lolcat");
-    str.insert(0, space, ' ');
-
-    for (int i=0; i<5;i++){
+    strl.insert(0, col - 2, '*');
+    cout<<strl<<endl;
+    col = (col- linea1->size() -4 )/2;
+    str.insert(0, col, ' ');
+    for (int i=0; i < 5;i++){
         cout<<"*"<<str <<  linea1[i] << str <<"*" <<endl;
     }
-    //cout<<linea1.size()<<endl;
-    /*for (int i = 0; i < tamanio_alto; i++)
-    {
-        
-        if(i>0 && i<6){
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<  linea1 << setw((tamanio_largo/2)-5) <<"*" <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<      <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<   <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<     <<endl;i-=-1;
-        cout<<"*"<<setw((tamanio_largo/2)-5)<<  <<endl;i-=-1;
-        }
-        if (i==0||i==tamanio_alto-1)
-        {
-            
-            for (int i = 0; i <tamanio_largo; i++)
-            {
-                cout<<"*";
+}
 
-            }
-            cout<<endl;
-        }
-        else
-        {
-            cout<<endl;
-        }
-        if(i==tamanio_alto-1){
-                
-        }
-        
-    }*/
+
+int main(void) {
+    winsize w = screenSize();//obtenemos las dimensiones de nuestra terminal
+    mensageSnake(w.ws_col);
+    
     
     return 0;
 };

e2dec251a1c7a3cc61ee78f61140b72a62d4843c
luist23 <00127717@uca.edu.sv>
2020-11-28 14:17:09 -0600

    mensajes adaptables

--- views/snakeprint.cpp
+++ views/snakeprint.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 void mensageSnake(int col){
-    string str,strl;
+    string str;
     string linea1[] = {
         "=====   ===    ==   =======   ==  ==   =====",
         "||      ||\\\\   ||   ||   ||   || //    ||   ",
@@ -13,8 +13,7 @@ void mensageSnake(int col){
         "   ||   ||  \\\\ ||   ||   ||   || \\\\    ||   " ,
         "=====   ==   ====   ==   ==   ==  ==   =====" 
     };
-    strl.insert(0, col - 2, '*');
-    cout<<strl<<endl;
+    
     col = (col- linea1->size() -4 )/2;
     str.insert(0, col, ' ');
     for (int i=0; i < 5;i++){
@@ -22,10 +21,27 @@ void mensageSnake(int col){
     }
 }
 
+void mensageSteep(int col){
+    string strl;
+    strl.insert(0, col - 4, ' ');
+    cout<<"*"<<strl<<"*"<<endl;
+}
+
+void mensageMargin(int col){
+    string strl;
+    strl.insert(0, col - 2, '*');
+    cout<<strl<<endl;
+}
+
 
 int main(void) {
     winsize w = screenSize();//obtenemos las dimensiones de nuestra terminal
+    
+    mensageMargin(w.ws_col);
+    mensageSteep(w.ws_col);
     mensageSnake(w.ws_col);
+    mensageSteep(w.ws_col);
+    mensageSteep(w.ws_col);
     
     
     return 0;

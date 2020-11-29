bcf6a771f90db76c489f32a83b672bf10766fe96
Drubico <00121117@uca.edu.sv>
2020-11-29 17:47:24 -0600

    (DR-37) ->> Mega Ultra responsive el Top

--- views/top.cpp
+++ views/top.cpp
@@ -13,9 +13,9 @@
 using namespace std;
 void top_game(myGame<nodeuserinfouser>* game);
 void welcometop();
-void ordenarparametros(int &colsize);
+void ordenarparametros(int colsize,int sobraparallegar);
 template<class T>
-void printtop(node<T>* front);
+void printtop(node<T>* front,int colsize,int sobraparallegar);
 void printtopaux(doubleLinked<nodeuserinfouser>* userlist);
 
 void top_game(myGame<nodeuserinfouser>* game){
@@ -36,60 +36,45 @@ void welcometop(){
 
 }
 template<class T>
-void printtop(node<T>* front){
-    int size=6;
-    int colsize=(w.ws_col)/size;
-    int sobra=0;// nose si size o 6 
-    
-    string nombres="";
-    //cout<<fwall;
+void printtop(node<T>* front,int colsize,int sobraparallegar){
     if(front) {
-        // sobra+=mensageLineSeccionAux(colsize,front->info.name);
-        // sobra+=mensageLineSeccionAux(colsize,to_string(front->info.coin));
-        // sobra+=mensageLineSeccionAux(colsize,to_string(front->info.nivel));
-        // sobra+=mensageLineSeccionAux(colsize,to_string(front->info.vidas));
-        // sobra+=mensageLineSeccionAux(colsize,to_string(front->info.puntajeContinuar));
-        // sobra+=mensageLineSeccionAux(colsize,to_string(front->info.puntaje));
-        //cout <<endl<<sobra<<endl;
-        sobra=w.ws_col-sobra;
-        //if(sobra>0){nombres.insert(0,sobra,' ');}
-        cout<<nombres<<"*"<<endl;
+        mensageLineSeccionAux(colsize,front->info.name,0,sobraparallegar);
+        mensageLineSeccionAux(colsize,to_string(front->info.coin),1,sobraparallegar);
+        mensageLineSeccionAux(colsize,to_string(front->info.nivel),1,sobraparallegar);
+        mensageLineSeccionAux(colsize,to_string(front->info.vidas),1,sobraparallegar);
+        mensageLineSeccionAux(colsize,to_string(front->info.puntajeContinuar),1,sobraparallegar);
+        mensageLineSeccionAux(colsize,to_string(front->info.puntaje),-1,sobraparallegar);
         mensageMargin(w.ws_col);
-        //cout <<endl<<sobra<<endl;
-
-        printtop(front->next);
+        printtop(front->next,colsize,sobraparallegar);
     }
-    else    
-        cout<<endl;
 }
 
 void printtopaux(doubleLinked<nodeuserinfouser>* userlist){
     int colsize = 0;
-    ordenarparametros(colsize);
-
-    printtop(userlist->front);
-}
-
-void ordenarparametros(int &colsize){
-    //funcion para que el top sea responsive
-    string espacios="";
-    int sobra=0;
-    //numero de columnas de la tabla
+     //numero de columnas de la tabla
     int columnas=6;
+     //funcion para que el top sea responsive
+    string espacios="";
     // tamanio de la pantalla (menos 2 porque se tiende a pasar al final)
     int tamaniotabla=w.ws_col-2;
     // calculamos cuantos elementos sobraran (no todas los tamanios de pantalla se pueden dividir exactos)
     int sobraparallegar=tamaniotabla%columnas;
     // tamanio de cada columna
     colsize=((tamaniotabla)/columnas);
+    ordenarparametros(colsize,sobraparallegar);
+
+    printtop(userlist->front,colsize,sobraparallegar);
+}
+
+void ordenarparametros(int colsize,int sobraparallegar){
     //cout<<": Tamanio de todo  : "<< tamaniotabla<<":| | Sobra al dividir las comunas :"<<sobraparallegar<<"| | COLSIZE :"<<colsize<<"| | tamanio tabla exacto : "<<colsize*columnas<<endl;
     // tamanio de la tabla entre sus columnas nos da el tamanio de cada columna 
     mensageMargin(w.ws_col);
-    sobra+=mensageLineSeccionAux(colsize,"Nombre",0,sobraparallegar);
-    sobra+=mensageLineSeccionAux(colsize,"Monedas",1,sobraparallegar);
-    sobra+=mensageLineSeccionAux(colsize,"Nivel",1,sobraparallegar);    
-    sobra+=mensageLineSeccionAux(colsize,"Vidas",1,sobraparallegar);
-    sobra+=mensageLineSeccionAux(colsize,"Puntaje Actual",1,sobraparallegar);
-    sobra+=mensageLineSeccionAux(colsize,"Puntaje Maximo",-1,sobraparallegar);
+    mensageLineSeccionAux(colsize,"Nombre",0,sobraparallegar);
+    mensageLineSeccionAux(colsize,"Monedas",1,sobraparallegar);
+    mensageLineSeccionAux(colsize,"Nivel",1,sobraparallegar);    
+    mensageLineSeccionAux(colsize,"Vidas",1,sobraparallegar);
+    mensageLineSeccionAux(colsize,"Puntaje Actual",1,sobraparallegar);
+    mensageLineSeccionAux(colsize,"Puntaje Maximo",-1,sobraparallegar);
     mensageMargin(w.ws_col);
 }

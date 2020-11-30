d27783e2af7915a2b2026e6d2ca5f4fba1ab7c9b
CristianAlfaro <00167917@uca.edu.sv>
2020-11-30 16:08:18 -0600

    reimplementacion de logica de movimienta + bug en velocidad

--- snake/snake.h
+++ snake/snake.h
@@ -54,7 +54,7 @@ void changeVelocity(myGame<nodeuserinfouser>* game){
 void snakeNew(myGame<nodeuserinfouser>* game, int fil, int col){
     game->snake.list->pushBack(nodeinfo(fil,col));
     if(game->snake.M[fil][col]== FOOD ){
-        changeVelocity(game);
+        //changeVelocity(game);
         game->snake.sizeSnake++;
         increasePoints(game);
         if(game->snake.sizeSnake == game->snake.sizeMax){

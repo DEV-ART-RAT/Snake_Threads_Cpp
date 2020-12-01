00571a5b9582a79704461e8c4a5e6b00a5363268
Drubico <00121117@uca.edu.sv>
2020-11-30 23:02:12 -0600

    (DR-69) -> MODO HISTORIA!(quitando un cout que subi sin querer)

--- views/historias.cpp
+++ views/historias.cpp
@@ -10,14 +10,13 @@ void mensajehistoria(int pos,int max,string histo[]);
 
 int history(void) {
     string historia[]={
-        "En el princio de la humanidad las personas vivian felizmente",
-        "Habian muchos trabajos pero el que mas llamaba la atencion era",
-        "Ser mago",
+        "En el princio de la humanidad las personas vivian felizmente,",
+        "Habian muchos trabajos pero el que mas llamaba la atencion era Ser mago,",
         "Por lo que muchos estudiaban magia negra",
-        "En la universidad de los buhos",
+        "En la universidad de los buhos,",
         "Eran felices hasta que un dia llego",
-        "El Poderoso ENXEL",
-        "Que vino a convertir a toda la raza humana en serpientes",
+        "El Poderoso ENXEL,",
+        "Que vino a convertir a toda la raza humana en Serpientes,",
         "Pero llegaste tu para combatirlo",
         "Asi que adelante Gana y vence a Enxel!!!"
     };
